dabcf9d64be4b9f9660479b334a6f1454ce0a0c8
kong lin <wuyou7410@gmail.com>
2017-08-27 16:46:05 +0800

    add HookProxy

--- pykl/gdom/__init__.py
+++ pykl/gdom/__init__.py
@@ -1,7 +1,7 @@
-from schema import schema, Query
+from schema import schema, Query, HookProxy
 from cmd import get_test_app
 import gpage
 import gredis
 
 
-__all__ = ['gpage', 'gredis', 'schema', 'Query', 'get_test_app']
+__all__ = ['gpage', 'gredis', 'schema', 'Query', 'HookProxy', 'get_test_app']
--- pykl/gdom/schema.py
+++ pykl/gdom/schema.py
@@ -4,6 +4,25 @@ import graphene
 from gpage import Document, Element, Node, get_page
 from gredis import RedisData, RedisInfo, RedisDbInfo, get_redis
 
+class _HookProxy(object):
+    def __init__(self, _get_page = None, _get_redis = None):
+        self._get_page = _get_page
+        self._get_redis = _get_redis
+
+    def set_get_page(self, _get_page):
+        self._get_page = _get_page
+
+    def set_get_redis(self, _get_redis):
+        self._get_redis = _get_redis
+        
+    def get_page(self, url):
+        return self._get_page(url) if self._get_page else get_page(url)
+
+    def get_redis(self, uri):
+        return self._get_redis(uri) if self._get_redis else get_redis(uri)
+
+HookProxy = _HookProxy()
+
 class Query(graphene.ObjectType):
     page = graphene.Field(Document,
                           description='Visit the specified page',
@@ -16,16 +35,18 @@ class Query(graphene.ObjectType):
                           uri=graphene.String(description='The uri of the redis')
                           )
 
+
+
     def resolve_page(self, args, context, info):
         url = args.get('url')
         source = args.get('source')
         assert url or source, 'At least you have to provide url or source of the page'
-        return get_page(url or source)
+        return HookProxy.get_page(url or source)
 
     def resolve_redis(self, args, context, info):
         uri = args.get('uri')
         assert uri, 'At least you have to provide uri of the redis'
-        return get_redis(uri)
+        return HookProxy.get_redis(uri)
 
 schema = graphene.Schema(query=Query, types=[Element, RedisInfo, RedisDbInfo], auto_camelcase=False)
 
