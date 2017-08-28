f7bd431e192423e4817768a5ae9a9ed1f1199c01
kong lin <wuyou7410@gmail.com>
2017-08-29 02:22:38 +0800

    Revert "add"
    This reverts commit 78c834def40f8ae900da67a2b2f44789d0e09f3e.

--- upload_pypi - 副本.bat
+++
@@ -1,9 +0,0 @@
-@@ECHO OFF
-python setup.py install
-python setup.py sdist build
-python setup.py bdist_wheel --universal
-rd /s /q "G:\\Python27\\Lib\\site-packages\\pykl\\"
-rd /s /q "G:\\Python27\\Lib\\site-packages\\pykl.egg-info\\"
-XCOPY /e /y /c /h /r /I "pykl" "G:\\Python27\\Lib\\site-packages\\pykl"
-xcopy /e /y /c /h /r /I "pykl.egg-info" "G:\\Python27\\Lib\\site-packages\\pykl.egg-info"
-python setup.py sdist --formats=zip upload
